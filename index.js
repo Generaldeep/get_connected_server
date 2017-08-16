0314874e8ccc24eb7199a4e57f86f58f39e2b37a
Gurdip Singh <gurdipsingh1990@gmail.com>
2017-08-16 13:41:12 -0700

    Added cors package and changed preflight routes optins

--- index.js
+++ index.js
@@ -1,9 +1,17 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const cors = require('cors');
 const app = express();
+
+const corsOptions = {
+  origin: true,
+  credentials: true
+}
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cors(corsOptions))  
+app.options('*', cors(corsOptions));
 
 require('./routes/simpleRoute')(app);
 require('./routes/jobs')(app);
