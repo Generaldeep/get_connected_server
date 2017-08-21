ff2a7b0bb92efbbcca3ef1772f6066102bc2faa8
Gurdip Singh <gurdipsingh1990@gmail.com>
2017-08-21 16:33:07 -0700

    V2.2

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 const firebase = require('firebase');
-// require('./services/passport');
+
 
 const app = express();
 
--- services/passport.js
+++ services/passport.js
@@ -1,31 +1,19 @@
-const passport = require('passport');
-const GoogleStrategy = require('passport-google-oauth20').Strategy;
-const FirebaseStrategy = require('passport-firebase-auth').Strategy;
+// const passport = require('passport');
+// const GoogleStrategy = require('passport-google-oauth20').Strategy;
+// const FirebaseStrategy = require('passport-firebase-auth').Strategy;
 // const keys = require('../config/keys');
 // const firebaseInfo =  require('../config/firebaseConfig');
-const firebase = require('firebase');
-
-passport.use(new GoogleStrategy(
-  {
-    clientID: keys.googleClientID,
-    clientSecret: keys.googleClientSecret,
-    callbackURL: '/auth/google/callback'
-  },
-  (accessToken, refreshToken, profile, done) => {
-    let user = {googleId: profile.id, name: profile.displayName}
-    let userKey = firebase.database().ref('users').push().key;
-    firebase.database().ref(`users/${userKey}`).set(user)
-  })
-);
-
-// passport.use(new FirebaseStrategy({
-//     firebaseProjectId: firebaseInfo.projectId,
-//     authorizationURL: 'http://localhost:5050',
+// const firebase = require('firebase');
+//
+// passport.use(new GoogleStrategy(
+//   {
+//     clientID: keys.googleClientID,
+//     clientSecret: keys.googleClientSecret,
 //     callbackURL: '/auth/google/callback'
 //   },
-//   function(accessToken, refreshToken, decodedToken, cb) {
-//     User.findOrCreate(..., function (err, user) {
-//       return cb(err, user);
-//     });
-//   }
-// ));
+//   (accessToken, refreshToken, profile, done) => {
+//     let user = {googleId: profile.id, name: profile.displayName}
+//     let userKey = firebase.database().ref('users').push().key;
+//     firebase.database().ref(`users/${userKey}`).set(user)
+//   })
+// );
